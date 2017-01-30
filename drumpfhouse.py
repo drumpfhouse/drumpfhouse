5e3495dccbb01b372345b2fb735e3cb2271ff179
Drumpfhouse <drumpfhouse@users.noreply.github.com>
2017-01-29 21:52:07 -0500

    Adding automation script

---
+++ drumpfhouse.py
@@ -0,0 +1,47 @@
+# torify wget -E -k -p --no-clobber https://www.whitehouse.gov/
+
+import os
+import fileinput
+
+def getHTMLFileList(targetpath):
+	htmlFiles = []
+	for dirname, dirpaths, dirfiles in os.walk(targetpath):
+		for file in dirfiles:
+			if file.endswith('.html'):
+				htmlFiles.append(os.path.join(dirname, file))
+
+	return htmlFiles
+
+def manageReplacement(filelist):
+	with fileinput.FileInput(fileList, inplace=True) as file:
+		try:
+			for line in file:
+				print(_replaceLine(line), end='')
+		except:
+			pass
+
+
+def _replaceLine(line):
+	replacementPatterns = [
+		('Trump', 'Drumpf'),	
+		('whitehouse.gov</title>', 'drumpfhouse.gov</title>'),
+		('https://www.whitehouse.gov', 'http://www.drumpfhouse.com'),
+		('The White House', 'The Drumpf House'),
+		('@WhiteHouse', '@DrumpfHouse'),
+		('content="whitehouse.gov"', 'content="drumpfhouse.com"'),
+		('twitter.com/POTUS', 'twitter.com/drumpfhouse'),
+		('twitter.com/whitehouse', 'twitter.com/drumpfhouse'),
+		('embed/VBgNoP-DXWo', 'embed/7ceLnMT0rps'),
+		('WHITE HOUSE', 'DRUMPF HOUSE')
+		]
+	for orig, new in replacementPatterns:
+		line = line.replace(orig, new)
+
+	return line
+
+
+if __name__ == '__main__':
+	WGETDIR = 'www.whitehouse.gov'
+	
+	fileList = getHTMLFileList(WGETDIR)
+	manageReplacement('fileList')
