07b04dd6b429c7abfb47fc392d84f981e51c9d12
Drumpfhouse <drumpfhouse@users.noreply.github.com>
2017-05-24 05:26:45 -0400

    Adding new error handling for non-UTF8 characters and new replacement text.

--- drumpfhouse.py
+++ drumpfhouse.py
@@ -28,17 +28,27 @@ def manageReplacement(fileList):
     with fileinput.FileInput(fileList, inplace=True) as file:
         try:
             for line in file:
-                print(_replaceLine(line), end='')
+                try:
+                        print(_replaceLine(line.encode(errors='ignore').decode()), end='')
+                except Exception as err:
+                        sys.stderr.write("Oops.  I shit the bed. {}".format(str(err)))
+                        continue
         except Exception as err:
-            sys.stderr.write("Oops.  I shit the bed.")
-            pass
+            sys.stderr.write("failed processing {}: {}".format(file.filename(), str(err)))
 
+            pass
 
 def _replaceLine(line):
     replacementPatterns = [
+        ('reaffirm America\'s global leadership, continue building key relationships with world leaders, and deliver a message of unity to America\'s friends and allies', 'exploit his position to pad his own pockets, suck up to dictatorial strong men, and cut sweetheart deals for his businesses.'),
+        ('shows respect for the people who pay the bills', 'shows he has no respect for the people who pay the bills'),
+        ('will provide opportunities for economic growth and creation','will abandon the poor, sacrifice the environment to Big Pollution, and shit on the best aspirations of our nation'),
+        ('Participate', 'Serve Drumpf'),
         ('Trump Speaks', 'Drumpf Lies'),
         ('Trump Makes Remarks', 'Drumpf Lies'),
         ('White House Press Briefing', 'Spicering the Press'),
+        ('Press Briefing', 'Spicering the Press'),
+        ('Remarks by', 'Lies by'),
         ('Trump', 'Drumpf'),
         ('whitehouse.gov</title>', 'drumpfhouse.gov</title>'),
         ('https://www.whitehouse.gov', 'http://www.drumpfhouse.com'),
@@ -49,13 +59,16 @@ def _replaceLine(line):
         ('twitter.com/whitehouse', 'twitter.com/drumpfhouse'),
         ('embed/VBgNoP-DXWo', 'embed/7ceLnMT0rps'),
         ('WHITE HOUSE', 'DRUMPF HOUSE'),
-        ('Let\'s Make America Great Again, Together', 'Let\'s Rape America Again, Together'),
+        ('Believe in yourselves. Believe in your future.<br />', 'Nothing matters but what glorifies me.'),
+        ('And believe, once more, in America.',''),
         ('The Movement Continues - The Work Begins!', 'Serve Me, You Ignorant Slobs!'),
         ('What\'s Happening', 'How We\'re Screwing You Over'),
         ('/profiles/forall/modules/custom/gov_whitehouse_www/images/icons/wh_logo_seal.png',
          '/dhAssets/WHlogo_trumpdoor.png'),
-        ('../sites/whitehouse.gov/files/45/POTUS_Speech2.jpg', '/dhAssets/thisWide.jpg'),
-        ('../sites/whitehouse.gov/files/45/FLOTUS_Melania1.jpg', '/dhAssets/melaniaAdminMain.jpg'),
+        ('../sites/whitehouse.gov/files/images/IMG_1086_0.JPG', '/dhAssets/thisWide.jpg'),
+
+        ('His campaign slogan was Make America Great Again, and that is exactly what he intends to do.', 'His campaign slogan was Make America Great Again, but that\'s not at all what he intends to do.'),
+        ('<img alt="First Lady Melania Drumpf" src="../sites/whitehouse.gov/files/images/inauguration.png">', '<img alt="Free Melania" src="/dhAssets/melaniaAdminMain.jpg">'),
         ('The Cabinet includes the Vice President and the heads of 15 executive departments',
          'The Cabinet includes Vladimir Putin, the Vice President and the heads of 15 executive departments'),
         ('and was born on April 26, 1970 in Slovenia.',
@@ -63,7 +76,10 @@ def _replaceLine(line):
         ('potential and will go on to exceed anything that it has achieved in the past',
          'potential to make him richer and he\'s going to exploit it for all it\'s worth'),
         ('been married to his wife, Melania', 'owned his slave wife, Melania'),
-        ('first-lady-michelle-obama.html', 'first-lady-melania-trump.html')
+        ('first-lady-michelle-obama.html', 'first-lady-melania-trump.html'),
+        ('Watch the President\'s first address to a Joint Session of Congress and get more information here.', 'Watch Drumpf\'s speechwriters do their best to make him sound like a rational humanbeing and not the jabbering shit-gibbon he is.'),
+        ('1600 Daily', 'Our Daily Email Blast of Bullshit'),
+        ('Obamacare has led to higher costs and fewer health insurance options for millions of Americans.', 'Obamacare has secured health care for millions of Americans, but we want to take it away from you.')
     ]
     for orig, new in replacementPatterns:
         line = line.replace(orig, new)
